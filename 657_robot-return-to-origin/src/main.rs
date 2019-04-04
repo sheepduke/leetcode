9ded60cadd40da6cd99ea0f2bf6e3164e19b744f
YUE Daian <yue_d@worksap.co.jp>
2019-04-04 12:51:58 +0800

    Solve #657

--- 657_robot-return-to-origin/src/main.rs
+++ 657_robot-return-to-origin/src/main.rs
@@ -1,3 +1,21 @@
+struct Solution {}
+
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        let (mut x, mut y) = (0, 0);
+        moves.chars().for_each(|ch| match ch {
+            'U' => y -= 1,
+            'D' => y += 1,
+            'L' => x -= 1,
+            'R' => x += 1,
+            _ => panic!("Unexpected character"),
+        });
+        x == 0 && y == 0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "UDR";
+    let result = Solution::judge_circle(String::from(input));
+    println!("Result: {}", result);
 }
