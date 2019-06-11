bad91412a8c1b9d764299b301660036559b2088c
YUE Daian <yue_d@worksap.co.jp>
2019-06-11 11:46:30 +0800

    feat: solve #3

--- src/3. Longest Substring Without Repeating Characters/main.cpp
+++ src/3. Longest Substring Without Repeating Characters/main.cpp
@@ -17,16 +17,43 @@ Example 3:
 
 Input: "pwwkew"
 Output: 3
-Explanation: The answer is "wke", with the length of 3. 
-             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Explanation: The answer is "wke", with the length of 3.  Note that the answer
+             must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-#include <set>
 #include <string>
+#include <iostream>
+#include <cassert>
 using namespace std;
 
 class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
+    if (s.empty()) {
+      return 0;
+    }
+    
+    auto max_length = 1;
+    for (int left = 0, right = 1; right < s.length(); right++) {
+      char current = s[right];
+      for (auto index = right - 1; index >= left; index--) {
+        if (current == s[index]) {
+          left = index + 1;
+        }
+      }
+      max_length = max(max_length, right - left + 1);
+    }
+    return max_length;
   }
+};
+
+int main() {
+  Solution solution;
+  assert(solution.lengthOfLongestSubstring("abcabcbb") == 3);
+  assert(solution.lengthOfLongestSubstring("bbbbb") == 1);
+  assert(solution.lengthOfLongestSubstring("pwwkew") == 3);
+  assert(solution.lengthOfLongestSubstring("abc") == 3);
+  
+  return 0;
 }
